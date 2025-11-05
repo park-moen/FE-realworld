a5e59a1620081f0df1e40926feec8cf8c1e94142
park-moen <57402711+park-moen@users.noreply.github.com>
2025-11-05 13:25:23 +0900

    feat: 로그인 기능 구현 및 관련 컴포넌트 추가 (#6)
    * feat: 로그인 기능 구현 및 관련 컴포넌트 추가
    
    - Login Api instance 생성
    - Login Schema 생성
    - Login 비지니스 로직(mutation) 구현
    - Login Form UI & Login Page UI 구현
    - Login Router 연결
    - Login Integration Test Red & Green
    
    * refactor: Login 테스트 및 UI에서 사용하지 않는 의존 패키지 로직 제거
    
    - login.ui.tsx에서 사용하지 않는 console.log 로직 제거
    - login.test.tsx에서 샤용하지 않는 react-router-dom의 data 메서드 의존성 제거

--- src/app/browser-router.tsx
+++ src/app/browser-router.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { pathKey } from '~shared/router';
-import { BaseRegisterForm } from '~features/session/register/register.ui';
 import { HomePage } from '~pages/home/Homepage';
 import { Layout } from '~pages/layouts/Layout';
+import LoginPage from '~pages/login/login-page.ui';
+import { RegisterPage } from '~pages/register/register-page.ui';
 
 export function BootstrappedRouter() {
   return <RouterProvider router={browserRouter} />;
@@ -16,7 +17,11 @@ const browserRouter = createBrowserRouter([
       { index: true, Component: HomePage },
       {
         path: pathKey.register,
-        Component: BaseRegisterForm,
+        Component: RegisterPage,
+      },
+      {
+        path: pathKey.login,
+        Component: LoginPage,
       },
     ],
   },
---
+++ src/features/session/login/login.test.tsx
@@ -0,0 +1,121 @@
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
+import type { MockedFunction } from 'vitest';
+import { api } from '~shared/api/api.instance';
+import type { UserDto } from '~shared/api/api.schemas';
+import { renderWithQueryClient } from '~shared/lib/test/test.lib';
+import type { LoginUser } from './login.schema';
+import { BaseLoginForm } from './login.ui';
+
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual<typeof import('react-router-dom')>('react-router-dom');
+
+  return {
+    ...actual,
+    useNavigate: vi.fn(),
+  };
+});
+
+vi.mock('~shared/api/api.instance', () => ({
+  api: { post: vi.fn() },
+}));
+
+const mockedUseNavigate = useNavigate as MockedFunction<typeof useNavigate>;
+const mockedApiPost = api.post as MockedFunction<typeof api.post>;
+
+describe('LoginForm', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should render login form', () => {
+    renderLoginForm();
+
+    expect(screen.getByPlaceholderText('Email')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
+  });
+
+  it('displays validation errors when form fields are invalid', async () => {
+    const { click, type } = renderLoginForm();
+
+    await type(screen.getByPlaceholderText('Email'), 'test');
+    await type(screen.getByPlaceholderText('Password'), 'test');
+    await click(screen.getByRole('button', { name: /sign in/i }));
+
+    await waitFor(() => {
+      expect(screen.getAllByRole('alert')).toHaveLength(2);
+    });
+  });
+
+  it('should call login mutation on valid form submission', async () => {
+    mockedApiPost.mockResolvedValue({});
+
+    const { click, type } = renderLoginForm();
+
+    await type(screen.getByPlaceholderText('Email'), mockLoginUser.email);
+    await type(screen.getByPlaceholderText('Password'), mockLoginUser.password);
+    await click(screen.getByRole('button', { name: /sign in/i }));
+
+    await waitFor(() => {
+      expect(mockedApiPost).toHaveBeenCalled();
+    });
+  });
+
+  it('should navigate to profile on successful login', async () => {
+    const navigate = vi.fn();
+
+    mockedUseNavigate.mockReturnValue(navigate);
+    mockedApiPost.mockResolvedValue({ data: mockUserDto });
+
+    const { click, type } = renderLoginForm();
+
+    await type(screen.getByPlaceholderText('Email'), mockLoginUser.email);
+    await type(screen.getByPlaceholderText('Password'), mockLoginUser.password);
+    await click(screen.getByRole('button', { name: /sign in/i }));
+
+    await waitFor(() => {
+      expect(navigate).toHaveBeenCalled();
+    });
+  });
+
+  it('should display error message on login failure', async () => {
+    mockedApiPost.mockRejectedValue(new Error('Request failed'));
+
+    const { click, type } = renderLoginForm();
+
+    await type(screen.getByPlaceholderText('Email'), 'test@example.com');
+    await type(screen.getByPlaceholderText('Password'), 'password123');
+    await click(screen.getByRole('button', { name: /sign in/i }));
+
+    await waitFor(() => {
+      expect(screen.getByText(/Request failed/i)).toBeInTheDocument();
+    });
+  });
+});
+
+function renderLoginForm() {
+  const user = userEvent.setup();
+  const renderResult = renderWithQueryClient(
+    <BrowserRouter>
+      <BaseLoginForm />
+    </BrowserRouter>,
+  );
+
+  return { ...user, ...renderResult };
+}
+
+const mockLoginUser: LoginUser = {
+  email: 'mockuser@xeample.com',
+  password: 'mockuserpassword',
+};
+
+const mockUserDto: UserDto = {
+  user: {
+    email: 'mockuser@example.com',
+    token: 'mock-jwt-token-12345',
+    username: 'mockuser',
+    bio: 'This is a mock bio of the user.',
+    image: 'https://example.com/mockuser-image.jpg',
+  },
+};
