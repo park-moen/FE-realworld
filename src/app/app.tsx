fb265ce6311e9bfee53c82f84aa6d5154f8533fa
park-moen <pmj6516@naver.com>
2025-12-03 19:55:01 +0900

    chore: TanStack React Query DevTools 추가
    개발 환경에서 쿼리 상태와 캐시를 시각적으로 모니터링할 수 있도록
    React Query DevTools를 추가합니다.
    
    수정 사항:
    - src/app/app.tsx
      - ReactQueryDevtools import 추가
      - <ReactQueryDevtools initialIsOpen={false} /> 컴포넌트 추가
        - QueryClientProvider 내부에 위치
        - 초기 상태: 닫혀있음 (필요할 때 열기)
    
    이점:
    - 개발 중 쿼리 상태 실시간 모니터링 가능
    - 캐시 상태, 요청 시간, 응답 데이터 확인
    - Redux DevTools와 유사한 개발 경험
    - 프로덕션에서는 자동으로 제외됨
    
    사용 방법:
    - 화면 우측 하단의 'TanStack Query' 아이콘 클릭
    - 쿼리 상태 확인 및 캐시 리셋 가능
    
    주의:
    - 개발 환경에서만 효과 있음
    - initialIsOpen={false}로 성능 영향 최소화

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { queryClient } from '~shared/queryClient';
@@ -11,6 +12,7 @@ export default function App() {
       <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
         <QueryClientProvider client={queryClient}>
           <BootstrappedRouter />
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </PersistGate>
     </ReduxProvider>
